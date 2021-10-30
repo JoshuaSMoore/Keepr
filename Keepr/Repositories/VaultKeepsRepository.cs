9770f1bc21e974da28e0f85de0c8a8bdaa6e4510
JoshuaSMoore <J_Moore23@hotmail.com>
2021-10-29 19:05:36 -0600

    Vault Keep in progress

---
+++ Keepr/Repositories/VaultKeepsRepository.cs
@@ -0,0 +1,23 @@
+using System;
+using Keepr.Models;
+
+namespace Keepr.Repositories
+{
+  public class VaultKeepsRepository
+  {
+    internal VaultKeep Create(VaultKeep vaultKeepData)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal VaultKeep GetById(int vaultKeepId)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void Delete(int vaultKeepId)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
